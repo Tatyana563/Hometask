1a845b1c8037f3193b449121dda6428d0f2ee05d
Tatyana563 <nikolaev.english@gmail.com>
2019-08-26 15:36:05 +0300

    new commit 26.08

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -41,23 +41,29 @@ public class Car implements Comparable<Car> {
         List<Car> list2= new ArrayList<Car>(set);
 
         for (int i = 0; i < list.size(); i ++) {
-            if(list2.get(i).year>list.get(i).year){
-                resultList.add(list2.get(i));
-            }
+for(int j=0;j<list2.size();j++) {
+    if (list2.get(j).year > list.get(i).year) {
+        resultList.add(list2.get(j));
+    }
+}
         }
         // ArrayList<Car> b = (ArrayList<Car>)resultList.clone();
 
         System.out.println(resultList);
     }
     public void compareСarYears(List<Car> list, Set<Car> set) {
-        List<Car> resultList = new ArrayList<Car>();
-
+        List<Car> resultList = new ArrayList<>();
+        resultList.addAll(0,list);
         for(Car c: set){
-           for(int i=0;i<list.size();i++){
-         if(c.year>(list.get(i).year)){
-               resultList.add(c);
-           }
-       }
+
+            for(Car w: list){
+
+                   if(c.year>(w.year)){
+                       resultList.add(c);
+                   }
+
+
+            }
        }
        System.out.println(resultList);
     }
@@ -79,15 +85,16 @@ public class Car implements Comparable<Car> {
 
     public static void main(String[] args) {
             Car c1 = new Car("Toyota","blue",2001);
-            Car c6 = new Car("Toyota","blue",2002);
+           // Car c6 = new Car("Toyota","blue",2002);
             Car c2 = new Car("Ford","green",2002);
             Car c3 = new Car("Nissan","red",2003);
-            Car c4 = new Car("Suzuki","blue",2008);
-            Car c5 = new Car("Renault","blue",2001);
+            Car c4 = new Car("Suzuki","blue",2004);
+            Car c5 = new Car("Renault","blue",2000);
             List<Car> carList = new ArrayList<Car>();
             Set<Car> carSet = new TreeSet<Car>();
-            carSet.add(c4);
-            carSet.add(c5);
+            carSet.add(c3);
+          //  carSet.add(c4);
+          //  carSet.add(c5);
        // carSet.add(c6);// would not be added
 
 
